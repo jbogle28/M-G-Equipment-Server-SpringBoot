08a2cc763ac4f2ccac8348bc55636a8ddc87f0dd
Jordan Bogle <jordanbogle23@gmail.com>
2026-04-09 05:05:48 -0500

    Update SecurityConfig.java

--- src/main/java/com/java/scheduler/config/SecurityConfig.java
+++ src/main/java/com/java/scheduler/config/SecurityConfig.java
@@ -1,3 +1,22 @@
+package com.java.scheduler.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor 
 public class SecurityConfig {
